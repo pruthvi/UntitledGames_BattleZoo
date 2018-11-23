c44350825c293d380ac98abca78d2c1a24983ce0
Pruthvi <pruthvi@users.noreply.github.com>
2018-11-22 20:03:22 -0500

    Added Menu Scenes

---
+++ Battlezoo/Assets/Scripts/PlayerInfo/PlayerInfo.cs
@@ -0,0 +1,50 @@
+﻿/*  Copyright (c) Pruthvi  |  http://pruthv.com  */
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PlayerInfo : MonoBehaviour {
+
+    #region Variables
+
+    public InputField nameInput;
+    public Text errorText;
+
+    string playerName;
+
+	#endregion
+
+	void Start ()
+	{
+        errorText.enabled = false;
+
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+   public void OnClick()
+    {
+        if(nameInput == null)
+        {
+            errorText.enabled = true;
+            errorText.text = "Player Name is Mandatory";
+            nameInput.Select();
+            nameInput.ActivateInputField();
+        }
+        else
+        {
+            playerName = nameInput.text;
+            PlayerPrefs.SetString("Player Name", playerName);
+            SceneManager.LoadScene("MainScene");
+
+        }
+
+
+    }
+
+}
