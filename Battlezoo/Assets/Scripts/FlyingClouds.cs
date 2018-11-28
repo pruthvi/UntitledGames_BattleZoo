8f301eff9dd079e8c7e545d307a33bcda6b5aa68
Pruthvi <pruthvi@users.noreply.github.com>
2018-11-27 20:10:20 -0500

    Working flying cloud

--- Battlezoo/Assets/Scripts/FlyingClouds.cs
+++ Battlezoo/Assets/Scripts/FlyingClouds.cs
@@ -2,36 +2,60 @@
 
 using UnityEngine;
 
-public class FlyingClouds : MonoBehaviour {
+public class FlyingClouds : MonoBehaviour
+{
 
     #region Variables
-    [Range(1.0f, 20.0f)]
+
+
+
     public float speed;
     private Rigidbody2D rb;
 
-	#endregion
 
-	void Start ()
-	{
+    private SpriteRenderer cloudImg;
+    public Color col;
+    private float Opacity;
 
-        speed = Random.Range(1.0f, 50.0f);
+    private float posY;
+
+
+    #endregion
+
+    void Start()
+    {
+
+       // speed = Random.Range(minSpeed, maxSpeed);
+
         rb = GetComponent<Rigidbody2D>();
+        cloudImg = GetComponent<SpriteRenderer>();
+        Opacity = Random.Range(0.01f, 0.80f);
+        Debug.Log("Opacity: " + Opacity);
 
 
-	}
-	
-	void Update ()
-	{
-        float speed = Random.Range(0.0f, 2.0f);
+        col.a = Opacity;
+
+        cloudImg.color = col;
+
+        posY = this.transform.position.y;
+
+    }
+
+    void Update()
+    {
+        //float speed = Random.Range(0.0f, 2.0f);
         Vector2 movement = new Vector2(-1, 0);
 
         this.GetComponent<Rigidbody2D>().velocity = movement * speed;
 
         //        Debug.Log("Cloud Speed :" + speed);
 
-        if(transform.position.x < -20.0f)
+        if (transform.position.x < -20.0f)
         {
-            Destroy(this.gameObject);
+
+            Vector2 newPos = new Vector2(20, posY);
+            this.transform.position = newPos;
+
         }
 
     }
--- Battlezoo/Assets/Scripts/GenerateClouds.cs
+++ Battlezoo/Assets/Scripts/GenerateClouds.cs
@@ -9,31 +9,58 @@ public class GenerateClouds : MonoBehaviour
 
     public GameObject cloud;
 
-    
-    
+    [Range(1, 50)]
+    public int maxClouds;
+    [Range(1, 50)]
+    public int minClouds;
+
+    [Range(1.0f, 10.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float minSpeed;
+
+
+    [Range(1.0f, 10.0f)]
+    public float maxScale;
+    [Range(1.0f, 20.0f)]
+    public float minScale;
+
+    public float maxY = 6;
+    public float minY = -2;
+
+    public float maxX = 20;
+    public float minX= -15;
+
     #endregion
 
     void Start()
     {
-        CreateCloud();
+        int N = Random.Range(minClouds, maxClouds);
+
+        CreateCloud(N);
 
     }
 
     void Update()
     {
+
     }
 
-    public void CreateCloud()
+    public void CreateCloud(int No)
     {
-        int No = Random.Range(8, 15);
         for (int i = 0; i < No; i++)
         {
                                            //   x range             y range
-            Vector2 position = new Vector2(Random.Range(-15, 20), Random.Range(-2, 6));
-            float scale = Random.Range(5.0f, 20.0f);
+            Vector2 position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+
+            float scale = Random.Range(minScale, maxScale);
             // Debug.Log("Random Scale:" + scale);
             GameObject gb = Instantiate(cloud, position, Quaternion.identity);
             gb.transform.localScale = new Vector2(scale, scale);
+            gb.AddComponent<FlyingClouds>();
+            gb.GetComponent<FlyingClouds>().speed = Random.Range(minSpeed, maxSpeed);
+
+
             // Debug.Log("Randomly Scale!");
         }
     }
