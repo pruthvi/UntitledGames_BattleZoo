a6dc576aeb65829700f75aed3a5ed8da3084eefc
jalpendesai <djalpen24@gmail.com>
2018-11-28 02:08:54 -0500

    Fixed Character Flipping & Bullet Reloading
    For now, Camel and Chicken can flip and shoot.
    Bullet will take 3 sec to reload, after every 6 shotos

--- Battlezoo/Assets/Scripts/Weapon/BarrelRotator.cs
+++ Battlezoo/Assets/Scripts/Weapon/BarrelRotator.cs
@@ -12,16 +12,22 @@ public class BarrelRotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
-        // if Character is Flipped, change the Direction of Barrel rotation
+        if (!isCharacterFlipped)
+        {
+            BarrelRotation(1);
+        }
+
         if (isCharacterFlipped)
         {
-            direction = -direction;
+            BarrelRotation(-1);
         }
-        
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+	}
+
+    void BarrelRotation(int dir)
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, (dir) * direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         rotation.eulerAngles = new Vector3(0, 0, Mathf.Clamp(rotation.eulerAngles.z, minAngle, maxAngle));
         //Check if mouse is in the 3rd or 4th quadrant
@@ -29,13 +35,18 @@ public class BarrelRotator : MonoBehaviour {
         {
             if (direction.x > 0)// if in 4th quadrant
             {
-                rotation.eulerAngles = new Vector3(0, 0, minAngle);
+                rotation.eulerAngles = new Vector3(0, 0, isCharacterFlipped ? maxAngle : minAngle);
             }
             else if (direction.x < 0)// if in 3rd quadrant
             {
-                rotation.eulerAngles = new Vector3(0, 0, maxAngle);
+                rotation.eulerAngles = new Vector3(0, 0, isCharacterFlipped ? minAngle : maxAngle);
             }
         }
+        if(dir == -1)
+        {
+            rotation = Quaternion.Inverse(rotation);
+        }
         transform.rotation = rotation;
-	}
+    }
+
 }
--- Battlezoo/Assets/Scripts/Weapon/Weapon.cs
+++ Battlezoo/Assets/Scripts/Weapon/Weapon.cs
@@ -16,11 +16,12 @@ public class Weapon : MonoBehaviour {
 
     // Private Variable
     private Animator anim;
+    public BulletController bulletController;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         anim = gameObject.GetComponent<Animator>();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,22 +29,53 @@ public class Weapon : MonoBehaviour {
         {
             Vector2 direction = fireBarrel.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            var objBullet = Instantiate(bullet, fireBarrel.position, Quaternion.AngleAxis(angle, Vector3.forward)) as GameObject;
-            BulletController bulletController = objBullet.GetComponent<BulletController>();
-            switch (travelMode)
+
+            if (bulletController._canShootBullet)
             {
-                case BulletTravelMode.Linear:
-                    objBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletController.speed;
-                    break;
-                case BulletTravelMode.WithForce:
-                //    bulletController.ApplyForce(direction * bulletController.speed, 3);
-                    break;
+                // Incrementing the Bullet Fired Counter
+                bulletController._bulletCount++;
+                Debug.Log(bulletController._bulletCount);
+
+                // Check Magazine and Reload, if needed
+                ReloadBullet(bulletController._bulletReloadTime);
+
+                // Instantiating Bullet in the barrel's direction
+                var objBullet = Instantiate(bullet, fireBarrel.position, Quaternion.AngleAxis(angle, Vector3.forward)) as GameObject;
+                switch (travelMode)
+                {
+                    case BulletTravelMode.Linear:
+                        objBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletController.speed;
+                        break;
+                    case BulletTravelMode.WithForce:
+                        bulletController.ApplyForce(direction * bulletController.speed, 3);
+                        break;
+                }
+                anim.SetBool("IsFiring", true);
+
             }
-            anim.SetBool("IsFiring", true);
         }
         else
         {
             anim.SetBool("IsFiring", false);
         }
 	}
+
+
+    //------------- Reloading Bullet Count--------------------//
+    public void ReloadBullet(float _bulletReloadTime)
+    {
+        if (bulletController._bulletCount >= bulletController._maxAmmo)
+        {
+            // Recharge the Ammo
+            StartCoroutine(ReloadBulletTimmer(_bulletReloadTime));
+        }
+    }
+    IEnumerator ReloadBulletTimmer(float _bulletReloadTime)
+    {
+        bulletController._canShootBullet = false;                // Player cannot Shoot any bullet
+        bulletController._bulletCount = 0;                       // Magazine Reloaded
+        yield return new WaitForSeconds(_bulletReloadTime);
+        bulletController._canShootBullet = true;                 // Player can Shoot bullets
+    }
 }
+
